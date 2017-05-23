3c57fe7f70999e77e7c2487b9775563668c6a156
Lindsay <lindsay777@gmail.com>
2017-05-24 01:37:40 +0800

    add select

--- src/pages/my_bonus_banks/my_bonus_banks.ts
+++ src/pages/my_bonus_banks/my_bonus_banks.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { ActionSheetController } from 'ionic-angular';
 
 @Component ({
   selector: 'page-my_bonus_banks',
@@ -9,8 +9,11 @@ import { NavController, NavParams } from 'ionic-angular';
 export class My_bonus_banksPage {
 
   items;
+  select_banks: string;
 
-  constructor(public navCtrl: NavController, public NavParams: NavParams){
+  constructor(public navCtrl: NavController,
+              public NavParams: NavParams,
+              public ActionSheetCtrl: ActionSheetController){
 
     this.items = NavParams.data.item;
     console.log(this.items);
@@ -18,6 +21,33 @@ export class My_bonus_banksPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad My_bonus_banksPage');
+  }
+
+  /*presentActionSheet(){
+    let actionSheet = this.ActionSheetCtrl.create({
+      title: 'Select a bank',
+      buttons: [
+        {
+          text: '花',
+          role: 'ff',
+          handler: () => {
+            console.log('ff clicked');
+          }
+        },
+        {
+         text: 'Cancel',
+         role: 'cancel',
+         handler: () => {
+           console.log('Cancel clicked');
+         }
+       }
+      ]
+    });
+    actionSheet.present
+  }*/
+
+  onChange(select_banks){
+    console.log("Selected bank", select_banks);
   }
 
 }
